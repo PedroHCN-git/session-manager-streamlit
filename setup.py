682f0bad47a6dd5b0be08986828ee58cead72d10
PedroHCN <pedro.hcorradini05@gmail.com>
2025-03-13 11:47:22 -0300

    version changed

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit-session-manager",
-    version="0.1.1",
+    version="0.1.1.dev",
     author="Pedro Henrique Corradini Nunes",
     author_email="pedro.hcorradini05@gmail.com",
     description="this component allows you to manager you session storage",
